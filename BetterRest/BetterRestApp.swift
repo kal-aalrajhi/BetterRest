97fa92a689124fdab9e97e97c1fe35d73ce5a3b0
Kal <kal.aalrajhi@gmail.com>
2023-06-09 10:36:38 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Dr Cpt Blackbeard on 6/9/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
