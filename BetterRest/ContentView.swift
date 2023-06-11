444d89a12329757dd207225610e104d0727ac226
Kal <kal.aalrajhi@gmail.com>
2023-06-11 08:37:12 -0400

    Add sleep prediction based off coreML pretrained sleep model

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -4,13 +4,17 @@
 //
 //  Created by Dr Cpt Blackbeard on 6/9/23.
 //
-
+import CoreML
 import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date.now
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -35,11 +39,43 @@ struct ContentView: View {
             .toolbar {
                 Button("Calculate", action: calculateBedtime)
             }
+            .alert(alertTitle, isPresented: $showingAlert) {
+                Button("Ok") {}
+            } message: {
+                Text(alertMessage)
+            }
         }
     }
 
     func calculateBedtime() {
-       
+        do {
+            // The default configuration (usually this is all you need...any more parameters are just super advanced)
+            let config = MLModelConfiguration()
+            // Model instance that reads in all the data we want (coffee intake, when you want to sleep etc...) and outputs a prediction
+            // We use Try because loading the model might fail (could be incompatable etc..)
+            let model = try SleepCalculator(configuration: config)
+            
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            let hour = (components.hour ?? 0) * 60 * 60 // hours -> minutes -> seconds
+            let minute = (components.minute ?? 0) * 60 // minutes -> seconds
+            let totalSeconds = hour + minute // total seconds from 0 AKA midnight
+            
+            // Prediction is how much sleep they need...this was not part of our training data, it's something totally new
+            let prediction = try model.prediction(wake: Double(totalSeconds), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your ideal bedtime is..."
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+//            print("wakeup: \(wakeUp)")
+//            print("prediction: \(prediction.actualSleep)")
+//            print("sleep time: \(sleepTime)")
+            
+        } catch {
+            alertTitle = "Error"
+            alertMessage = "Sorry, unable to calculate your bedtime."
+        }
+        
+        showingAlert = true
     }
 }
 
