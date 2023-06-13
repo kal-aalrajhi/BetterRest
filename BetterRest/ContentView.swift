43ada45b404ee7e74f38ca879f6d6c4c7235b67b
Kal <kal.aalrajhi@gmail.com>
2023-06-13 11:02:09 -0400

    DRY UI Code

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,76 +11,17 @@ struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
-    
-    @State private var alertTitle = ""
     @State private var suggestedBedtime = ""
-    @State private var showingAlert = false
     
     static var defaultWakeTime: Date {
         var components = DateComponents()
         components.hour = 7
         components.minute = 0
         return Calendar.current.date(from: components) ?? Date.now
-        
     }
     
-    var body: some View {
-        NavigationView {
-            Form {
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-                    
-                    // Even though we hide the label here, we still put in a helpful title for screen readers
-                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                        .labelsHidden()
-                }
-                .onChange(of: wakeUp) { _ in
-                    calculateBedtime()
-                }
-                
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("Desired amount of sleep")
-                        .font(.headline)
-                    
-                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                }
-                .onChange(of: sleepAmount) { _ in
-                    calculateBedtime()
-                }
-                
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("Daily coffee intake")
-                        .font(.headline)
-                    
-                    Picker("Cups", selection: $coffeeAmount) {
-                        ForEach(1...20, id: \.self) { num in
-                            Text("\(num)")
-                        }
-                    }
-                }
-                .onChange(of: coffeeAmount) { _ in
-                    calculateBedtime()
-                }
-                
-                Section {
-                    Text(suggestedBedtime)
-                } header: {
-                    Text("Suggested Bedtime")
-                }
-                .font(.title2)
-                .onAppear(perform: calculateBedtime)
-            }
-            .navigationTitle("BetterRest")
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("Ok") {}
-            } message: {
-                Text("Error")
-            }
-        }
-    }
-
-    func calculateBedtime() {
+    // Calculated property
+    var sleepResults: String {
         do {
             // The default configuration (usually this is all you need...any more parameters are just super advanced)
             let config = MLModelConfiguration()
@@ -97,10 +38,42 @@ struct ContentView: View {
             let prediction = try model.prediction(wake: Double(totalSeconds), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             
             let sleepTime = wakeUp - prediction.actualSleep
-            suggestedBedtime = sleepTime.formatted(date: .omitted, time: .shortened)
+            return sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
-            alertTitle = "Error, unable to calculate your bedtime."
-            showingAlert = true
+            return "Error"
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section("When do you want to wake up?") {
+                    // Even though we hide the label here, we still put in a helpful title for screen readers
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                
+                Section("Desired amount of sleep") {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                }
+                
+                Section("Daily coffee intake") {
+                    Picker("Cups", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) {
+                            Text(String($0))
+                        }
+                    }
+                }
+                
+                Section("Suggested Bedtime") {
+                    Text(sleepResults)
+                        .font(.title)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                    
+                }
+            }
+            .navigationTitle("BetterRest")
+            .font(.headline)
         }
     }
 }
